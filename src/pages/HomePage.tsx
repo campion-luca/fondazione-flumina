27418d8b4891c9b29ad4dd66ac483f7c78c9dc40
campion-luca <campionluca3@gmail.com>
2025-09-30 10:55:39 +0200

    modifica testo per presentazione

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -12,23 +12,18 @@ const HomePage = () => {
             </h1>
 
             {/* Immagine */}
-            <img src={logo} alt="Logo Fondazione Flumina" className="h-45 w-45 mb-4 mt-3" />
+            <img src={logo} alt="Logo Fondazione Flumina" className="h-50 w-50 mb-4 mt-3" />
 
             {/* Sottotitolo */}
-            <p className="text-lg mb-6 text-center px-4 mx-10 mt-3 text-[#1b4a54]">
-                Fondazione dedicata alla promozione di progetti di welfare aziendale, benessere organizzativo e sviluppo sostenibile delle comunità. 
-                La Fondazione Flumina si impegna a creare valore condiviso, contribuendo al miglioramento della qualità della vita delle persone e 
-                al rafforzamento del tessuto sociale e territoriale.
-                <br /><br />
-                Fondazione Flumina nasce con l’obiettivo di sviluppare progetti innovativi di welfare aziendale e territoriale, 
-                mettendo al centro il benessere dei singoli e delle comunità in cui operano le imprese. La fondazione opera come una vera 
-                e propria piattaforma di sostegno al benessere collettivo, promuovendo iniziative culturali, formative, sociali e ambientali che favoriscono 
-                l’equilibrio tra impresa, persona e comunità.
-                <br /><br />
-                Attraverso la collaborazione con aziende, enti locali, associazioni e istituzioni, Fondazione Flumina sviluppa percorsi di crescita sostenibile, 
-                capaci di coniugare efficienza organizzativa e responsabilità sociale. L’attenzione al capitale umano, alla valorizzazione delle competenze e alla 
-                qualità della vita lavorativa rappresenta un elemento centrale della missione della fondazione, così come la volontà di creare comunità più coese, 
-                resilienti e attente al futuro.
+            
+            <p className="text-lg mb-6 text-center px-4 mx-15 mt-3 text-[#1b4a54]">
+                Fondazione Flumina nasce a sostegno del benessere collettivo, promuovendo iniziative culturali, formative e di inclusione che
+                favoriscono l’equilibrio tra impresa, persona e comunità. Opera con l’obiettivo di sviluppare progetti innovativi di welfare
+                aziendale e per la promozione e valorizzazione territoriale.
+                <br />
+
+                La Fondazione Flumina pone particolare attenzione alla crescita culturale e del tessuto sociale mettendo al centro delle proprie attività
+                l’ascolto dei bisogni del territorio, trasformandoli in progettualità concrete capaci di generare valore condiviso.
             </p>
 
             {/* Work in progress */}
